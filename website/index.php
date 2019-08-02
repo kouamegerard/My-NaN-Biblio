38ac962b74f3eea04a83e3c8c946d45b8a391862
KOUAME GERARD KODJO <32105212+kouamegerard@users.noreply.github.com>
2019-08-02 13:08:27 +0200

    Project
    Voila tu peux prendre

---
+++ website/index.php
@@ -0,0 +1,71 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Template Website</title>
+	<link rel="stylesheet" href="css/app.css">
+</head>
+<body>
+	
+	<header class="main-header">
+		<div class="header-nav">
+			<div class="logo"><h2>LOGO</h2></div>
+			<ul class="menu-lists">
+				<li class="menu-list-item"><a href="#" class="item">Home</a></li>
+				<li class="menu-list-item"><a href="#" class="item">Store</a></li>
+				<li class="menu-list-item"><a href="#" class="item">Contact</a></li>
+			</ul>
+		</div>
+
+		<div class="foot-header">
+			<div class="hr-white"></div>
+
+			<span class="round-circle active"></span>
+			<span class="round-circle"></span>
+			<span class="round-circle"></span>
+			<span class="round-circle"></span>
+			<span class="round-circle"></span>
+
+		</div>
+	</header>
+
+	<section id="section-A">
+		<div class="img-info">
+			<div class="info">
+				Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+				Omnis quasi dolor cum, sint cupiditate alias, harum minus 
+				quos nemo odit dignissimos. A totam neque odit commodi cupiditate
+				autem assumenda explicabo.
+			</div>
+			<div class="img">
+				<img src="" alt="" class="first-image">
+			</div>
+		</div>
+		<div class="img-info">
+			<div class="img">
+				<img src="" alt="" class="second-image">
+			</div>
+			<div class="info">
+				Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+				Laudantium quod a neque ratione sit sint consequatur repellendus,
+				voluptate, illo culpa asperiores eos, nihil veniam iste quibusdam
+				at odit quas accusantium!
+			</div>
+		</div>
+	</section>
+
+	<footer class="main-footer">
+		<div class="footer-info">
+			<div class="footer-list">Logo</div>
+			<div class="footer-list">Menu Secondaire</div>
+			<div class="footer-list">Contact & Network</div>
+		</div>
+		<div class="copyright">
+			&copy; Copyright LOGO, 2019 All Right Reserved
+		</div>
+	</footer>
+
+
+
+</body>
+</html>
